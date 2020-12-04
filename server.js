7a4189777a410644006fda70647aece9d51d8822
radualexvornicu <radualex.vornicu@gmail.com>
2020-12-04 15:19:10 +0100

    routes in server

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.get("/", (req, res) => {
 const db = require("./models");
 db.sequelize.sync();
 
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;

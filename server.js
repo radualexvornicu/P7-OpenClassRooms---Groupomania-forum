bbb0b66bcd94dccd4d55f64397027e216d8ecf95
radualexvornicu <radualex.vornicu@gmail.com>
2020-12-07 17:59:43 +0100

    name corection

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ db.sequelize.sync();
 
 require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
-require("./app/routes/categories.routes")(app);
+require("./routes/categories.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;

176382ba550127541b2509a670fdb0c3d29da348
radualexvornicu <radualex.vornicu@gmail.com>
2020-12-17 17:30:14 +0100

    cat topics with user creator

--- src/views/BoardAdmin.vue
+++ src/views/BoardAdmin.vue
@@ -26,7 +26,10 @@
           >
             Refresh Categorie list
           </button>
-          <button class="m-3 btn btn-sm btn-info" @click="addCategorie()">
+          <button
+            class="m-3 btn btn-sm btn-info"
+            @click="submittedSaveCat = true"
+          >
             Add New Categorie
           </button>
         </div>
@@ -35,60 +38,186 @@
         <div class="col-md-6">
           <h4>Categories List</h4>
           <div
-            class="card border-info mb-3"
+            type="button"
+            class="card border-info mb-3 btn-light"
             :class="{ active: indexCat == currentIndexCat }"
             v-for="(categories, indexCat) in categories"
             :key="indexCat"
-            @click="setActiveCategorie(categories, indexCat)"
+            @click="setActiveCategorie(categories, indexCat); getUserNameCat(categories.userId);"
           >
             <div class="card-header">
-              <p>{{ categories.catname }}</p>
+              <h5>{{ categories.catname }} </h5>
+              
             </div>
-            <div class="card-body" v-show="indexCat == currentIndexCat">
-              <h5 class="card-title">Categorie Description</h5>
-              <p class="card-text">{{ categories.catdescription }}</p>
+            <div class="card-body" v-if="indexCat == currentIndexCat">
+              <h6 class="card-title font-weight-bold">Categorie Description </h6>
+              
+              <h6 class="card-text">{{ categories.catdescription }}</h6>
+              <cite><h6>created by {{userNameCat}}</h6></cite> 
             </div>
             <div>
               <button
                 type="button"
-                class="btn btn-warning"
-                v-show="indexCat == currentIndexCat"
+                class="btn btn-warning m-1 p-0"
+                v-if="indexCat == currentIndexCat"
+                @click="submittedEditCat = true"
               >
                 Edit
               </button>
               <button
                 type="button"
-                class="btn btn-success"
-                v-show="indexCat == currentIndexCat"
+                class="btn btn-success m-1 p-0"
+                v-if="indexCat == currentIndexCat"
               >
                 Add Topic
               </button>
               <button
                 type="button"
-                class="btn btn-danger"
-                v-show="indexCat == currentIndexCat"
+                class="btn btn-danger m-1 p-0"
+                v-if="indexCat == currentIndexCat"
+                @click="removeCategorie(currentCategorie.id)"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+          <div class="submit-form">
+            <div v-if="submittedEditCat">
+              <div class="form-group">
+                <label for="catname">Categorie Name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="catname"
+                  required
+                  v-model="categorie.catname"
+                  name="catname"
+                />
+              </div>
+
+              <div class="form-group">
+                <label for="catdescription">Categorie Description</label>
+                <input
+                  class="form-control"
+                  id="catdescription"
+                  required
+                  v-model="categorie.catdescription"
+                  name="catdescription"
+                />
+              </div>
+
+              <button
+                @click="editCategorie(currentCategorie.id)"
+                class="btn btn-success"
+              >
+                Submit
+              </button>
+            </div>
+            <div v-if="submittedSaveCat">
+              <div class="form-group">
+                <label for="catname">Categorie Name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="catname"
+                  required
+                  v-model="categorie.catname"
+                  name="catname"
+                />
+              </div>
+
+              <div class="form-group">
+                <label for="catdescription">Categorie Description</label>
+                <input
+                  class="form-control"
+                  id="catdescription"
+                  required
+                  v-model="categorie.catdescription"
+                  name="catdescription"
+                />
+              </div>
+
+              <button @click="saveCategorie()" class="btn btn-success">
+                Submit
+              </button>
+            </div>
+            <div v-if="submittedSaveTopic">
+              <div class="form-group">
+                <label for="catname">Categorie Name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="catname"
+                  required
+                  v-model="categorie.catname"
+                  name="catname"
+                />
+              </div>
+
+              <div class="form-group">
+                <label for="catdescription">Categorie Description</label>
+                <input
+                  class="form-control"
+                  id="catdescription"
+                  required
+                  v-model="categorie.catdescription"
+                  name="catdescription"
+                />
+              </div>
+
+              <button @click="saveCategorie()" class="btn btn-success">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6" v-if="currentCategorie">
+          <h4>Topics List</h4>
+          <div
+            type="button"
+            class="card border-info mb-3 btn-light"
+            :class="{ active: indexTopic == currentIndexTopic }"
+            v-for="(topics, indexTopic) in topics"
+            :key="indexTopic"
+            @click="setActiveTopic(topics, indexTopic); getUserNameTopic(topics.userId)"
+          >
+            <div class="card-header">
+              <h5>{{ topics.topicsubject }}</h5>
+            </div>
+            <div class="card-body" v-if="indexTopic == currentIndexTopic">
+              <h6 class="card-title font-weight-bold">Topic Thead</h6>
+              <h6 class="card-text">Active thred length - {{posts.length}}</h6>
+              <cite><h6>created by {{userNameTopic}}</h6></cite>
+            </div>
+            <div>
+              <button
+                type="button"
+                class="btn btn-warning m-1 p-0"
+                v-if="indexTopic == currentIndexTopic"
+                @click="submittedEditCat = true"
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                class="btn btn-success m-1 p-0"
+                v-if="indexTopic == currentIndexTopic"
+              >
+                Add Topic
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger m-1 p-0"
+                v-if="indexTopic == currentIndexTopic"
+                @click="removeTopic(currentTopic.id)"
               >
                 Delete
               </button>
             </div>
           </div>
         </div>
-
-        <div class="col-md-6" v-show="currentCategorie">
-          <h4>Topics List</h4>
-          <ul class="list-group">
-            <li
-              class="list-group-item"
-              :class="{ active: indexTopic == currentIndexTopic }"
-              v-for="(topics, indexTopic) in topics"
-              :key="indexTopic"
-              @click="setActiveTopic(topics, indexTopic)"
-            >
-              {{ topics.topicsubject }}
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-6" v-show="currentTopic">
+        <div class="col-md-6" v-if="currentTopic">
           <h4>Posts Thread</h4>
           <ul class="list-group">
             <li
@@ -101,29 +230,7 @@
               {{ posts.postcontent }}
             </li>
           </ul>
-          <div class="submit-form">
-            <div v-if="!submittedPost">
-              <div class="form-group">
-                <label for="postcontent">Post Content</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="postcontent"
-                  required
-                  v-model="post.postcontent"
-                  name="postcontent"
-                />
-              </div>
-
-              <button @click="savePost" class="btn btn-success">Submit</button>
             </div>
-
-            <div v-else>
-              <h4>You submitted successfully!</h4>
-              <button class="btn btn-success" @click="newPost">Add</button>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   </div>
@@ -137,9 +244,10 @@ import PostsDataService from "../services/PostsDataService";
 const user = JSON.parse(localStorage.getItem("user"));
 
 export default {
-  name: "User",
+  name: "Admin",
   data() {
     return {
+      indexCat: "",
       categories: [],
       currentCategorie: null,
       currentIndexCat: -1,
@@ -152,17 +260,54 @@ export default {
       currentPost: null,
       currentIndexPost: -1,
       postcontent: "",
+      categorie: {
+        id: null,
+        catname: "",
+        catdescription: "",
+        userId: "",
+      },
       post: {
         id: null,
         postcontent: "",
         topicId: "",
         userId: "",
       },
+      userNameCat: "",
+      userNameTopic: "",
       submittedPost: false,
+      submittedSaveCat: false,
+      submittedEditCat: false,
+      submittedDeleteCat: false,
+      submittedSaveTopic: false,
     };
   },
 
   methods: {
+    getUserNameCat(id){
+      UserService.getUserName(id)
+      .then((response) => {
+        this.userNameCat = response.data.username;
+        console.log(response.data);
+      }).catch((e) => {
+          console.log(e);
+        });
+    },
+    getUserNameTopic(id){
+      UserService.getUserName(id)
+      .then((response) => {
+        this.userNameTopic = response.data.username;
+        console.log(response.data);
+      }).catch((e) => {
+          console.log(e);
+        });
+    },
+    submittedAll(){
+      this.submittedPost = false;
+      this.submittedSaveCat= false;
+      this.submittedEditCat= false;
+      this.submittedDeleteCat= false;
+      this.submittedSaveTopic = false;
+    },
     retrieveCategories() {
       CategoriesDataService.getAll()
         .then((response) => {
@@ -174,7 +319,12 @@ export default {
         });
     },
     refreshCategorieList() {
+      this.submittedAll();
       this.retrieveCategories();
+      this.currentTopic = null;
+      this.currentIndexTopic = -1;
+      this.currentPost = null;
+      this.currentIndexPost = -1;
       this.currentCategorie = null;
       this.currentIndexCat = -1;
     },
@@ -191,15 +341,30 @@ export default {
       this.retrieveTopics(catID);
     },
 
-    removeAllCategories() {
-      CategoriesDataService.deleteAll()
+    removeCategorie(catId) {
+      console.log(catId);
+      var r = confirm("Press a button!");
+      if (r == true) {
+        console.log("You pressed OK!");
+        var r2 = confirm("Press a button AGAIN!");
+        if (r2 == true) {
+          console.log("You pressed OK AGAIN!");
+        CategoriesDataService.delete(catId)
         .then((response) => {
           console.log(response.data);
-          this.refreshList();
+
+          this.refreshCategorieList();
         })
         .catch((e) => {
           console.log(e);
         });
+        }else {
+        console.log("You pressed ABORTED!");
+      }
+      } else {
+        console.log("You pressed CANCEL!");
+      }
+      /*   */
     },
 
     searchCatname() {
@@ -256,6 +421,45 @@ export default {
         .then((response) => {
           this.posts = response.data;
           console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    editCategorie(catId) {
+      var data = {
+        catname: this.categorie.catname,
+        catdescription: this.categorie.catdescription,
+        userId: user.id,
+      };
+      console.log(data);
+      var id = catId;
+      console.log(id);
+      CategoriesDataService.update(id, data)
+        .then((response) => {
+          this.categorie.id = response.data.id;
+          console.log(response.data);
+          this.submittedEditCat = false;
+          this.refreshCategorieList();
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    saveCategorie() {
+      var data = {
+        catname: this.categorie.catname,
+        catdescription: this.categorie.catdescription,
+        userId: user.id,
+      };
+      console.log(data);
+
+      CategoriesDataService.create(data)
+        .then((response) => {
+          this.categorie.id = response.data.id;
+          console.log(response.data);
+          this.submittedSaveCat = false;
+          this.refreshCategorieList();
         })
         .catch((e) => {
           console.log(e);
